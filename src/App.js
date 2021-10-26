8317cedc86d2197b88036a7797e1952c730ed4c1
Dmitry Shagurov <shagurovda@gmail.com>
2021-10-26 20:55:28 +0300

    html and scss for header added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ function App() {
       <header>
         <div className="logo-container">
           <div className="logo-container__image">
-            <img src="../public/images/logo.svg" alt="pizza"></img>
+            <img src="./images/logo.svg" alt="pizza"></img>
           </div>
           <div className="logo-container__text">
             <h1 className="logo-label">REACT PIZZA</h1>
             <p className="logo-description">most delicious pizza in the universe</p>
           </div>
         </div>
-        <div className="cart-button"></div>
+        <div className="cart-button">
+          <div className="cart-button__cost">520 ₽</div>
+          <div className="cart-button__separator"></div>
+          <div className="cart-button__image">
+            <img src="./images/cart.svg" alt="cart"></img>
+          </div>
+          <div className="cart-button__inputs">3</div>
+        </div>
       </header>
     </div>
   );
