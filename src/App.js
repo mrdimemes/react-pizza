484223b354257b317c2450c57764c068d8c7a9bb
Dmitry Shagurov <shagurovda@gmail.com>
2021-10-26 19:01:11 +0300

    html for header logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import './styles/css/App.css'
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <header>
+        <div className="logo-container">
+          <div className="logo-container__image">
+            <img src="../public/images/logo.svg" alt="pizza"></img>
+          </div>
+          <div className="logo-container__text">
+            <h1 className="logo-label">REACT PIZZA</h1>
+            <p className="logo-description">most delicious pizza in the universe</p>
+          </div>
+        </div>
+        <div className="cart-button"></div>
+      </header>
+    </div>
   );
 }
 
