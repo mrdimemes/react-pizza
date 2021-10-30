5b9a851ac60a2c435b49574e6ca2da8cb61b7bb2
Dmitry Shagurov <shagurovda@gmail.com>
2021-10-30 13:33:23 +0300

    rewritten header in BEM

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './styles/css/App.css'
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="logo-container">
-          <div className="logo-container__image">
-            <img src="./images/logo.svg" alt="pizza"></img>
+
+      <header className="header">
+        
+        <div className="header__logo">
+          <div className="header__logo-image-container">
+            <img className="image" src="./images/logo.svg" alt="pizza"></img>
           </div>
-          <div className="logo-container__text">
-            <h1 className="logo-label">REACT PIZZA</h1>
-            <p className="logo-description">most delicious pizza in the universe</p>
+          <div className="header__logo-text">
+            <h1 className="header__logo-label">REACT PIZZA</h1>
+            <p className="header__logo-description">most delicious pizza in the universe</p>
           </div>
         </div>
-        <div className="cart-button">
-          <div className="cart-button__cost">520 ₽</div>
-          <div className="cart-button__separator"></div>
-          <div className="cart-button__image">
-            <img src="./images/cart.svg" alt="cart"></img>
+
+        <div className="header__cart-button">
+          <div className="header__cart-button-cost">520 ₽</div>
+          <div className="header__cart-button-separator"></div>
+          <div className="header__cart-button-icon-container">
+            <img className="image" src="./images/cart.svg" alt="cart"></img>
           </div>
-          <div className="cart-button__inputs">3</div>
+          <div className="header__cart-button-inputs-counter">3</div>
         </div>
       </header>
 
