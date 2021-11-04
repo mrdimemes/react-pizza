68e0dbb6f872867f087bf9fb764490e7065f4b1e
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-04 18:11:31 +0300

    db.json fetching

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
-import '../styles/scss/App.scss'
+import React from 'react';
+import { Route } from 'react-router';
+
+import '../styles/scss/App.scss';
 
 import { Header } from './';
 import { MainPage } from '../pages';
-import { Route } from 'react-router';
 
 
 function App() {
+  const [pizzasArray, setPizzasArray] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/db.json")
+      .then((response) => response.json())
+      .then((json) => setPizzasArray(json.pizzas));
+  }, []);
+
+
   return (
     <div className="App">
       <Header />
