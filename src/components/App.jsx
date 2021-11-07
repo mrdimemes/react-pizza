e6e1734ca8de2f9c1c9c0bd0375bfa0ad5f10a4e
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-07 16:40:09 +0300

    App component was connected to Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import { connect } from 'react-redux';
 
 import '../styles/scss/App.scss';
 
 import { Header } from './';
 import { MainPage } from '../pages';
+import { setItems, setTypes, setSizes, setCategories } from '../redux/slices/pizzas';
 
 
-function App() {
-  const [pizzasArray, setPizzasArray] = React.useState([]);
-  const [pizzaTypes, setPizzaTypes] = React.useState([]);
-  const [pizzaSizes, setPizzaSizes] = React.useState([]);
-  const [pizzaCategories, setPizzaCategories] = React.useState([]);
-
-
+function App( {dispatch} ) {
 
   React.useEffect(() => {
     fetch("http://localhost:3000/db.json")
       .then((response) => response.json())
       .then((json) => {
-        setPizzaCategories(json.pizzaCategories);
-        setPizzaTypes(json.pizzaTypes);
-        setPizzaSizes(json.pizzaSizes);
-        setPizzasArray(json.pizzas);
+        dispatch(setCategories(json.pizzaCategories));
+        dispatch(setTypes(json.pizzaTypes));
+        dispatch(setSizes(json.pizzaSizes));
+        dispatch(setItems(json.pizzas));
       });
+      // eslint-disable-next-line
   }, []);
 
 
@@ -33,10 +30,11 @@ function App() {
       <Routes>
         <Route exact path="/" element={ 
           <MainPage 
-            categories={pizzaCategories} 
-            pizzaSizes={pizzaSizes}
-            pizzaTypes={pizzaTypes}
-            pizzas={pizzasArray}
+            // temporary placeholders
+            categories={["1", "2"]} 
+            pizzaSizes={[1, 2, 3]}
+            pizzaTypes={[1, 2]}
+            pizzas={[]}
           /> 
         } /> 
       </Routes>
@@ -44,4 +42,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect()(App);
