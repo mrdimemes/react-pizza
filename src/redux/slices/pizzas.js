fcceec3af83a8eb726e2dd89f9f8bf0e2cf6fea1
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-07 18:32:09 +0300

    Action for sort pizzas was added

--- src/redux/slices/pizzas.js
+++ src/redux/slices/pizzas.js
@@ -11,6 +11,7 @@ const pizzasSlice = createSlice({
   name: 'pizzas',
   initialState,
   reducers: {
+
     setItems(state, action) {
       state.items = action.payload
     },
@@ -22,10 +23,28 @@ const pizzasSlice = createSlice({
     },
     setCategories(state, action) {
         state.categories = action.payload
+    },
+
+    sortBy(state, action) {
+      state.items.sort((a, b) => {
+        switch (action.payload) {
+          case 'popularity':
+            return a.rating - b.rating;
+          case 'price':
+            return (
+              a.price[a.availableTypes[0]][a.availableSizes[0]] - 
+              b.price[b.availableTypes[0]][b.availableSizes[0]]
+            );
+          case 'alphabet':
+            return a.pizzaLabel.localeCompare(b.pizzaLabel);
+          default:
+            return 0;
+        }
+      })
     }
   }
 })
 
-export const { setItems, setTypes, setSizes, setCategories } = pizzasSlice.actions
+export const { setItems, setTypes, setSizes, setCategories, sortBy } = pizzasSlice.actions
 
 export default pizzasSlice.reducer
