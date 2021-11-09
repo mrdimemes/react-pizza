622201e871cb94e1ee09e7f575529d0f92da4522
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-09 22:23:13 +0300

    Cart-item block styles

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -36,39 +36,19 @@ function CartPage() {
                     <p className="Cart-item__description">thin dough, 30 cm.</p>
                   </div>
                 </div>
-                
-                <div className="Cart-item__counter">
-                  <div className="Cart-item__decrease-button">-</div>
-                  <p className="Cart-item__counter-state">1</p>
-                  <div className="Cart-item__encrease-button">+</div>
-                </div>
 
-                <div className="Cart-item__price">10 $</div>
-                <div className="Cart-item__clear-button"></div>
-              </div>
-
-              <div className="Cart__item Cart-item">
-                
-                <div className="Cart-item__info">
-                  <div className="Cart-item__image-container">
-                    <img className="image" src="" alt="pizza" />
+                <div className="Cart-item__interactive-bar">
+                  <div className="Cart-item__counter">
+                    <div className="Cart-item__decrease-button button button_theme_main-bordered">-</div>
+                    <p className="Cart-item__counter-state">1</p>
+                    <div className="Cart-item__encrease-button button button_theme_main-bordered">+</div>
                   </div>
-                  <div className="Cart-item__text">
-                    <h3 className="Cart-item__label">Mega Pizza 1337</h3>
-                    <p className="Cart-item__description">thin dough, 30 cm.</p>
-                  </div>
-                </div>
-                
-                <div className="Cart-item__counter">
-                  <div className="Cart-item__decrease-button">-</div>
-                  <p className="Cart-item__counter-state">1</p>
-                  <div className="Cart-item__encrease-button">+</div>
-                </div>
 
-                <div className="Cart-item__price">10 $</div>
-                <div className="Cart-item__clear-button"></div>
+                  <div className="Cart-item__price">10 $</div>
+                  <div className="Cart-item__clear-button button button_theme_weak-bordered"></div>
+                </div>
               </div>
-
+              
             </div>
 
             <div className="Cart__footer">
