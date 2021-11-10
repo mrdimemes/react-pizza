9b5e3e6f06aa4c07fc984e7c33f6ecc5756a18c3
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-10 20:24:18 +0300

    Button integration into Cart & CartItem

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,55 +2,56 @@ import React from 'react'
 import cartSvg from '../assets/images/dark-cart.svg';
 import trashSvg from '../assets/images/trash.svg';
 
-import { CartItem } from '../components'
+import { Button, CartItem } from '../components'
 
 function Cart() {
-    return (
-        <div className="Cart">
+  return (
+    <div className="Cart">
 
-            <div className="Cart__header interactive-bar">
-
-              <div className="Cart__section-label">
-                <div className="Cart__section-label-icon-container">
-                  <img className="image" src={cartSvg} alt="cart" />
-                </div>
-                <h1 className="Cart__section-label-text section-label">Cart</h1>
-              </div>
-              
-              <div className="Cart__clear-button button button_theme_weak">
-                <div className="Cart__clear-button-icon-container">
-                  <img className="image" src={trashSvg} alt="trash" />
-                </div>
-                <p className="Cart__clear-button-text">Clear cart</p>
-              </div>
-
-            </div>
-
-
-            <div className="Cart__items">
-
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              
-            </div>
-
-            <div className="Cart__footer">
-              <div className="Cart__counter">
-                Total entries: <span className="Cart__counter-state">3 pcs.</span>
-              </div>
-
-              <div className="Cart__price">
-                Order price: <span className="Cart__price-state">30 $</span>
-              </div>
-            </div>
-
-            <nav className="Cart__navigation interactive-bar">
-              <div className="Cart__back-button button button_shape_rounded button_theme_weak-bordered">Back</div>
-              <div className="Cart__pay-button button button_shape_rounded button_theme_main-colored">Pay now</div>
-            </nav>
-
+      <div className="Cart__header interactive-bar">
+        
+        <div className="Cart__section-label">
+          <div className="Cart__section-label-icon-container">
+            <img className="image" src={cartSvg} alt="cart" />
           </div>
+          <h1 className="Cart__section-label-text section-label">Cart</h1>
+        </div>
+
+        <Button className="Cart__clear-button" theme="weak">
+          <div className="Cart__clear-button-icon-container">
+            <img className="image" src={trashSvg} alt="trash" />
+          </div>
+          <p className="Cart__clear-button-text">Clear cart</p>
+        </Button>
+
+
+      </div>
+
+      <div className="Cart__items">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+
+      <div className="Cart__footer">
+        <div className="Cart__counter">
+          Total entries: <span className="Cart__counter-state">3 pcs.</span>
+        </div>
+        <div className="Cart__price">
+          Order price: <span className="Cart__price-state">30 $</span>
+        </div>
+      </div>
+
+      <nav className="Cart__navigation interactive-bar">
+        <Button className="Cart__back-button" shape="rounded" theme="weak-bordered">
+          Back 
+        </Button>
+        <Button className="Cart__pay-button" shape="rounded" theme="main-colored">
+          Pay now
+        </Button>
+      </nav>
+
+    </div>
     )
 }
 
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
+import { Button } from '../components'
+
 
 function CartItem() {
-    return (
-        <div className="Cart__item Cart-item">
-                <div className="Cart-item__info">
-                  <div className="Cart-item__image-container">
-                    <img className="image" src="" alt="pizza" />
-                  </div>
-                  <div className="Cart-item__text">
-                    <h3 className="Cart-item__label">Mega Pizza 1337</h3>
-                    <p className="Cart-item__description">thin dough, 30 cm.</p>
-                  </div>
-                </div>
+  return (
+    <div className="Cart__item Cart-item">
+      <div className="Cart-item__info">
+        <div className="Cart-item__image-container">
+          <img className="image" src="" alt="pizza" />
+        </div>
+        <div className="Cart-item__text">
+          <h3 className="Cart-item__label">Mega Pizza 1337</h3>
+          <p className="Cart-item__description">thin dough, 30 cm.</p>
+        </div>
+      </div>
 
-                <div className="Cart-item__interactive-bar">
-                  <div className="Cart-item__counter">
-                    <div className="Cart-item__decrease-button button button_theme_main-bordered">-</div>
-                    <p className="Cart-item__counter-state">1</p>
-                    <div className="Cart-item__encrease-button button button_theme_main-bordered">+</div>
-                  </div>
+      <div className="Cart-item__interactive-bar">
+        <div className="Cart-item__counter">
+          <Button className="Cart-item__decrease-button" theme="main-bordered" children="-" />
+          <p className="Cart-item__counter-state">1</p>
+          <Button className="Cart-item__encrease-button" theme="main-bordered" children="+" />
+        </div>
 
-                  <div className="Cart-item__price">10 $</div>
-                  <div className="Cart-item__clear-button button button_theme_weak-bordered">×</div>
-                </div>
-              </div>
-    )
+        <div className="Cart-item__price">10 $</div>
+        <Button className="Cart-item__clear-button" theme="weak-bordered" children="×" />
+      </div>
+    </div>
+  )
 }
 
 export default CartItem
