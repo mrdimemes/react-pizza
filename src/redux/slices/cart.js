609a38e922292d5605c482878162ac305fceb708
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-14 18:52:50 +0300

    Comments and code cleanup for Redux slices

--- src/redux/slices/cart.js
+++ src/redux/slices/cart.js
@@ -1,4 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+
+
+// State signature:
+//
+// entries: array of objects.
+//   Entry object signature: {
+//     id: integer > 0 - product identificator.
+//     imageUrl: string or undefined - path to the product image.
+//     label: string - product name.
+//     type: integer > 0 - index of types array (pizzas Redux slice).
+//     size: integer > 0 - index of sizes array store (pizzas Redux slice).
+//     price: number > 0 - product price for selected configuration.
+//     count: integer > 0 - number of products the with same configuration
+//       in the cart.
+//   }
+// entriesCount: integer >= 0 - total count the of shopping cart entries.
+// entriesPrice: number > 0 - total price of the shopping cart entries.
 
 const initialState = {
   entries: [],
@@ -10,6 +27,12 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+
+
+    // Action for add a new entry into the entries state.
+    // Action payload must contain the entry object (see signature upper).
+    // The count property of the entry object will always be 1.
+    // It is optional to specify it in the object.
 
     addEntry(state, action) {
       let enrtyInState = false;
@@ -36,6 +59,16 @@ const cartSlice = createSlice({
       state.entriesPrice += action.payload.price;
     },
 
+
+    // Action for remove the entry from the entries state.
+    // Action payload must contain the entry object (see signature upper).
+    // The entry object must contain the id, type, size and price properties.
+    // Other properties are optional.
+    //
+    // If there are several identical items with the given configuration
+    // in the entries state, then the action will only
+    // decrease the count property of such an entry by 1.
+
     removeEntry(state, action) {
       const entryIndex = state.entries.findIndex((entry) => {
         if (
@@ -59,6 +92,13 @@ const cartSlice = createSlice({
         state.entriesPrice -= action.payload.price;
       }
     },
+
+
+    // Action for remove all of entries with given configuration
+    // from the entries state.
+    // Action payload must contain the entry object (see signature upper).
+    // The entry object must contain the id, type, size and price properties.
+    // Other properties are optional.
 
     removeAllEntries(state, action) {
       const entryIndex = state.entries.findIndex((entry) => {
@@ -92,6 +132,11 @@ const cartSlice = createSlice({
   }
 })
   
-export const {addEntry, removeEntry, removeAllEntries, clearCart} = cartSlice.actions
-  
-export default cartSlice.reducer
+export const {
+  addEntry,
+  removeEntry,
+  removeAllEntries,
+  clearCart
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/redux/slices/pizzas.js
+++ src/redux/slices/pizzas.js
@@ -1,12 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+
+
+// State signature:
+//
+// types: array of strings, each string represents an available pizza type.
+// sizes: array of numbers, each number represents an available pizza size.
+// categories: array of strings, each string represents a pizza category.
+// items: array of pizza objects.
+//   Pizza object signature: {
+//     id: integer >= 0 - product identificator.
+//     imageUrl: string or undefined - path to the product image.
+//     pizzaLabel: string - product name.
+//     availableTypes: non empty array of integers >= 0 - contain indexes of
+//       types state, each index represents the available type
+//       for a specific product.
+//     availableSizes: non empty array of integers >= 0 - contain indexes of
+//       sizes state, each index represents the available size
+//       for a specific product.
+//     prices: array of arrays of numbers - the matrix of prices for
+//       any product configuration (price = prices[type][size]).
+//     categories: non empty array of integers >= 0 - contain indexes of
+//       categories state, each index represents one of the categories
+//       for a specific product.
+//     rating: number.
+//   }
+// shownItems: array of integers >= 0 - list of products identificators.
+//   Only pizzas whose id's are stored in the shownItems state
+//   will be rendered in the gallery.
+// sortOptions: array of strings, each string represents
+//   an available sort option for products.
+// isLoaded: boolean - logical flag indicating whether data has been loaded 
+//   from the server or not.
+// isSorted: boolean - logical flag indicating whether data has been already 
+//   sorted or not.
 
 const initialState = {
   types: [],
   sizes: [],
-  categories: ["all"],
+  categories: ['all'],
   items: [],
   shownItems: [],
-  sortOptions: ["popularity", "price", "alphabet"],
+  sortOptions: ['popularity', 'price', 'alphabet'],
   isLoaded: false,
   isSorted: false
 };
@@ -31,7 +65,7 @@ const pizzasSlice = createSlice({
     setCategories(state, action) {
       state.categories = action.payload
     },
-    
+
     itemsLoaded(state) {
       state.isLoaded = true
     },
@@ -51,7 +85,7 @@ const pizzasSlice = createSlice({
             return b.rating - a.rating;
           case 'price':
             return (
-              a.prices[a.availableTypes[0]][a.availableSizes[0]] - 
+              a.prices[a.availableTypes[0]][a.availableSizes[0]] -
               b.prices[b.availableTypes[0]][b.availableSizes[0]]
             );
           case 'alphabet':
@@ -65,16 +99,16 @@ const pizzasSlice = createSlice({
   }
 })
 
-export const { 
-  setItems, 
-  setShownItems, 
-  setTypes, 
-  setSizes, 
-  setCategories, 
-  sortBy, 
+export const {
+  setItems,
+  setShownItems,
+  setTypes,
+  setSizes,
+  setCategories,
+  sortBy,
   itemsLoaded,
   itemsSorted,
   itemsNotSorted
-} = pizzasSlice.actions
+} = pizzasSlice.actions;
 
-export default pizzasSlice.reducer
+export default pizzasSlice.reducer;
