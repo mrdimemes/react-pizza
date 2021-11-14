5b45e68b695073b5725e8de7c5d801c39adbf384
Dmitry Shagurov <shagurovda@gmail.com>
2021-11-14 17:01:13 +0300

    Comments and code cleanup for pages components

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,27 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import { connect } from 'react-redux';
-import { Cart, EmptyCart } from '../components'
+import { Cart, EmptyCart } from '../components';
 
 
-function CartPage( {entriesCount} ) {
+// Component for cart page representation.
+// If the cart is empty, CartPage will display the EmptyCart component.
+// Otherwise, it will be the Cart component.
+//
+// All props comes from Redux storage (cart slice).
+// Direct determination of component's props isn't necessary.
+//
+// Component props:
+//
+// entriesCount: integer >= 0 - total count the of shopping cart entries.
+//   Need for rendering decision.
+
+function CartPage({ entriesCount }) {
   const content = () => {
     if (entriesCount) {
       return <Cart />;
     } else {
       return (
-        <div className="App__empty-cart-wrapper">
+        <div className='App__empty-cart-wrapper'>
           <EmptyCart />
         </div>
       );
     }
   }
   return (
-    <div className="App__wrapper">
-      <section className="App__cart-section">
-        {content()}
+    <div className='App__wrapper'>
+      <section className='App__cart-section'>
+        { content() }
       </section>
     </div>
   )
 }
+
+CartPage.propTypes = {
+  entriesCount: PropTypes.number.isRequired
+};
 
 const mapStateToProps = (state) => {
   return {
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,32 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+import React from 'react';
 
 import { CategoriesMenu, SortMenu, PizzaGallery } from '../components/';
 
 
 function MainPage() {
-    return (
-      <div className="App__wrapper">
-        <div className="interactive-bar App__interactive-bar">
-          <CategoriesMenu />
-          <SortMenu />
-        </div>
-
-        <section className="App__gallery-section">
-          <h1 className="App__gallery-section-label section-label">All pizzas</h1>
-          
-          <PizzaGallery />
-          
-        </section>
+  return (
+    <div className='App__wrapper'>
+      <div className='interactive-bar App__interactive-bar'>
+        <CategoriesMenu />
+        <SortMenu />
       </div>
-    )
-}
 
-MainPage.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  pizzaTypes: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  pizzaSizes: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  pizzas: PropTypes.arrayOf(PropTypes.object)
-};
+      <section className='App__gallery-section'>
+        <h1 className='App__gallery-section-label section-label'>
+          All pizzas
+        </h1>
+
+        <PizzaGallery />
+
+      </section>
+    </div>
+  )
+}
 
 export default MainPage;
